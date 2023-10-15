8f76a1ed24729a9c3660af8f8feeecf81583a7f1
Jin <jttang29@gmail.com>
2023-10-15 12:18:30 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const port = 3000
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const app = express()
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
 
 app.get('/', (req, res) => {
   res.send('This is my Restaurant List.')
