c3dbc1b1870a1691df1e722325f6b556ad44fd08
Jin <jttang29@gmail.com>
2023-10-16 00:06:38 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require("body-parser")
 const Restaurant = require('./models/restaurant')
 const port = 3000
 
@@ -28,11 +29,23 @@ db.once("open", () => {
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }));
 app.set("view engine", "hbs");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   Restaurant.find()
   .lean()
   .then(restaurants => res.render('index', { restaurants }))
   .catch(error => console.error(error))
+})
+
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/restaurants', (req, res) => {
+  Restaurant.create(req.body)
+  .then(() => res.redirect('/'))
+  .catch(err => console.log(err))
 })
 
 app.listen(port, () => {
