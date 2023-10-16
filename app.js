d914ee6cebfda4202b350a692b81ad99e4ec9da2
Jin <jttang29@gmail.com>
2023-10-17 01:28:58 +0800

    feat: add style

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ db.once("open", () => {
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }));
 app.set("view engine", "hbs");
 
+app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(methodOverride('_method'))
