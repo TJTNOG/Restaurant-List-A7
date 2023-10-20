8810e2e31965d97fe3854ea24ef18cb8da8d5dd7
Jin <jttang29@gmail.com>
2023-10-20 20:49:26 +0800

    feat: add search function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -10,4 +10,20 @@ router.get("/", (req, res) => {
     .catch((error) => console.error(error));
 });
 
+router.get('/search', (req, res) => {
+  const keywords = req.query.keywords
+  const keyword = keywords.trim().toLowerCase()
+  if(!keywords) {
+    res.redirect('/')
+  }
+  Restaurant.find({})
+    .lean()
+    .then(restaurantsData => {
+      const filterRestaurantData = restaurantsData.filter(data =>
+        data.name.toLowerCase().includes(keyword) ||
+        data.category.includes(keyword))
+        res.render('index', {restaurants: filterRestaurantData, keywords})
+    })
+})
+
 module.exports = router
