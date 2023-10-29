dd7a908e67f117d5940629e6c0bbb75054026716
Jin <jttang29@gmail.com>
2023-10-29 23:22:31 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const session = require("express-session");
 const exphbs = require('express-handlebars')
 const bodyParser = require("body-parser")
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 const routes = require("./routes");
 require("./config/mongoose");
@@ -28,7 +29,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 
 usePassport(app)
-
+usePassport(app);
+app.use(flash()); // 掛載套件
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  res.locals.user = req.user;
+  res.locals.success_msg = req.flash("success_msg"); // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash("warning_msg"); // 設定 warning_msg 訊息
+  next();
+});
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated();
