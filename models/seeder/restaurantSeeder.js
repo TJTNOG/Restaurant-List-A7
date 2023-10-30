a23aebc96ec229e594e8bb944c7fe188be567369
Jin <jttang29@gmail.com>
2023-10-30 22:46:09 +0800

    modify restaurantSeeder

--- models/seeder/restaurantSeeder.js
+++ models/seeder/restaurantSeeder.js
@@ -1,21 +1,71 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const Restaurant = require("../restaurant");
-const restaurantList = require('../../restaurant.json').results
-
+const restaurantList = require('../../restaurant.json')
+const User = require('../users')
 const db = require('../../config/mongoose')
 
-db.once("open", () => {
-  for(let i = 0; i < restaurantList.length; i++) {
-    Restaurant.create({
-    name: `${restaurantList[i].name}`,
-    name_en: `${restaurantList[i].name_en}`,
-    category: `${restaurantList[i].category}`,
-    image: `${restaurantList[i].image}`,
-    location: `${restaurantList[i].location}`,
-    phone: `${restaurantList[i].phone}`,
-    google_map: `${restaurantList[i].google_map}`,
-    rating: `${restaurantList[i].rating}`,
-    description: `${restaurantList[i].description}`,
-  })
-  }
-});
+const SEED_USER_01 = {
+  email: "user1@example.com",
+  password: "12345678",
+  restaurantOwns: [1, 2, 3], //陣列內容為restaurant的id
+};
+const SEED_USER_02 = {
+  email: "user2@example.com",
+  password: "12345678",
+  restaurantOwns: [4, 5, 6],
+};
 
+db.once("open", () => {
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER_01.password, salt))
+    .then((hash) =>
+      User.create({
+        email: SEED_USER_01.email,
+        password: hash,
+      })
+    )
+    .then((user) => {
+      const userId = user._id;
+      return Promise.all(
+        Array.from({ length: SEED_USER_01.restaurantOwns.length }, (_, i) =>
+          Restaurant.create({
+            ...restaurantList.results[SEED_USER_01.restaurantOwns[i] - 1],
+            userId,
+          })
+        )
+      );
+    })
+    .then(() => {
+      console.log("User1 is done.");
+      //process.exit()
+    });
+
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER_02.password, salt))
+    .then((hash) =>
+      User.create({
+        email: SEED_USER_02.email,
+        password: hash,
+      })
+    )
+    .then((user) => {
+      const userId = user._id;
+      return Promise.all(
+        Array.from({ length: SEED_USER_02.restaurantOwns.length }, (_, i) =>
+          Restaurant.create({
+            ...restaurantList.results[SEED_USER_02.restaurantOwns[i] - 1],
+            userId,
+          })
+        )
+      );
+    })
+    .then(() => {
+      console.log("User2 is done.");
+      process.exit();
+    });
+});
